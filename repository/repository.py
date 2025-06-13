8ae1d1a945760720d1c39b5928fa284181885045
Yumax-panda <yuma0wall@gmail.com>
2025-06-13 14:28:15 +0900

    わざわざクラス使う必要がないので削除

--- repository/repository.py
+++ repository/repository.py
@@ -8,55 +8,35 @@ from sqlalchemy.ext.asyncio import create_async_engine
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncEngine
 
-__all__ = ("Config", "Repository")
+__all__ = ("get_repository", "Repository")
 
 
-class Config:
-    __slots__ = ("user", "password", "host_name", "port", "db_name", "ssl_ca_path")
+def get_repository(
+    user: str,
+    password: str,
+    host_name: str,
+    port: int,
+    db_name: str,
+    ssl_ca_path: str | None = None,
+) -> Repository:
+    dsn = f"mysql+aiomysql://{user}:{password}@{host_name}:{port}/{db_name}"
 
-    if TYPE_CHECKING:
-        user: str
-        password: str
-        host_name: str
-        port: int
-        db_name: str
-        ssl_ca_path: str | None
+    if ssl_ca_path:
+        ssl_context = ssl.create_default_context(cafile=ssl_ca_path)
+        engine = create_async_engine(
+            dsn,
+            echo=True,
+            pool_pre_ping=True,
+            connect_args={"ssl": ssl_context},
+        )
+    else:
+        engine = create_async_engine(
+            dsn,
+            echo=True,
+            pool_pre_ping=True,
+        )
 
-    def __init__(
-        self,
-        user: str,
-        password: str,
-        host_name: str,
-        port: int,
-        db_name: str,
-        ssl_ca_path: str | None = None,
-    ) -> None:
-        self.user = user
-        self.password = password
-        self.host_name = host_name
-        self.port = port
-        self.db_name = db_name
-        self.ssl_ca_path = ssl_ca_path
-
-    def get_repository(self) -> Repository:
-        dsn = f"mysql+aiomysql://{self.user}:{self.password}@{self.host_name}:{self.port}/{self.db_name}"
-
-        if self.ssl_ca_path:
-            ssl_context = ssl.create_default_context(cafile=self.ssl_ca_path)
-            engine = create_async_engine(
-                dsn,
-                echo=True,
-                pool_pre_ping=True,
-                connect_args={"ssl": ssl_context},
-            )
-        else:
-            engine = create_async_engine(
-                dsn,
-                echo=True,
-                pool_pre_ping=True,
-            )
-
-        return Repository(engine)
+    return Repository(engine)
 
 
 class Repository:
