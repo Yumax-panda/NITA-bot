f56ff9587fb3b881340c73ecc4ddb8c9d200c5ec
Yumax-panda <yuma0wall@gmail.com>
2025-06-24 16:18:00 +0900

    display_timeのテストを追加

--- tests/test_utils_time.py
+++ tests/test_utils_time.py
@@ -1,6 +1,6 @@
 import pytest
 
-from utils.time import input_text_to_time_ms
+from utils.time import display_time, input_text_to_time_ms
 
 
 @pytest.mark.parametrize(
@@ -8,3 +8,10 @@ from utils.time import input_text_to_time_ms
 )
 def test_input_text_to_time_ms(text: str, expected: int) -> None:
     assert input_text_to_time_ms(text) == expected
+
+
+@pytest.mark.parametrize(
+    ("time_ms", "expected"), [(121065, "2:01.065"), (10123, "0:10.123")]
+)
+def test_display_time(time_ms: int, expected: str) -> None:
+    assert display_time(time_ms) == expected
