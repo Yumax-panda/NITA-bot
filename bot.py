84edf1b38b6958971723b4e16e5d9220d291bd03
Yumax-panda <yuma0wall@gmail.com>
2025-06-27 03:52:44 +0900

    repositoryを導入

--- bot.py
+++ bot.py
@@ -1,27 +1,39 @@
 from __future__ import annotations
 
+import logging
 from typing import TYPE_CHECKING
 
 import discord
 from discord.ext import commands
 
+if TYPE_CHECKING:
+    from repository import Repository
+
 __all__ = ("Bot",)
+
+logger = logging.getLogger(__name__)
 
 
 class Bot(commands.Bot):
     if TYPE_CHECKING:
         _token: str
+        repo: Repository
 
-    def __init__(self, bot_token: str, command_prefix: str) -> None:
+    def __init__(self, bot_token: str, repo: Repository, command_prefix: str) -> None:
         super().__init__(
             command_prefix=command_prefix,
             intents=discord.Intents.default(),
             case_insensitive=True,
         )
+        self.repo = repo
         self._token = bot_token
 
+    async def setup_hook(self) -> None:
+        await self.repo.sync()
+        logger.info("Repository is ready.")
+
     async def on_ready(self) -> None:
-        print(f"Logged on as {self.user}")
+        logger.info(f"Logged on as {self.user}")
 
     def run(self) -> None:
         super().run(self._token)
--- launcher.py
+++ launcher.py
@@ -1,14 +1,34 @@
+import logging
+
 from dependency_injector import containers, providers
 from dependency_injector.wiring import Provide, inject
+from discord.utils import setup_logging
 
 from bot import Bot
+from repository import get_repository
+
+setup_logging()
+logging.getLogger("discord").setLevel(logging.WARN)
 
 
 class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
 
+    repo = providers.Singleton(
+        get_repository,
+        user=config.db_user,
+        password=config.db_password,
+        host_name=config.db_hostname,
+        port=config.db_port,
+        db_name=config.db_name,
+        ssl_ca_path=config.ssl_ca_path,
+    )
+
     bot = providers.Singleton(
-        Bot, bot_token=config.bot_token, command_prefix=config.command_prefix
+        Bot,
+        bot_token=config.bot_token,
+        repo=repo,
+        command_prefix=config.command_prefix,
     )
 
 
@@ -19,6 +39,12 @@ def main(bot: Bot = Provide[Container.bot]) -> None:
 
 if __name__ == "__main__":
     container = Container()
+    container.config.db_user.from_env("DB_USERNAME", default="root")
+    container.config.db_password.from_env("DB_PASSWORD", default="password")
+    container.config.db_hostname.from_env("DB_HOSTNAME", default="localhost")
+    container.config.db_port.from_env("DB_PORT", as_=int, default=3306)
+    container.config.db_name.from_env("DB_NAME", default="mkbot")
+    container.config.ssl_ca_path.from_env("SSL_CA_PATH", default=None)
     container.config.bot_token.from_env("BOT_TOKEN", required=True)
     container.config.command_prefix.from_env("COMMAND_PREFIX", default="!")
     container.wire(modules=[__name__])
