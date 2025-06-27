e56c1c20717c2bf96d24e1598599875b27e3c052
Yumax-panda <yuma0wall@gmail.com>
2025-06-27 18:13:18 +0900

    cogを読み込ませる & app_commandを反映させるようにした

--- bot.py
+++ bot.py
@@ -7,30 +7,56 @@ import discord
 from discord.ext import commands
 
 if TYPE_CHECKING:
+    from discord import AppInfo, User
+
     from repository import Repository
 
 __all__ = ("Bot",)
 
 logger = logging.getLogger(__name__)
+extensions = [
+    "cogs.time_trial",
+]
 
 
 class Bot(commands.Bot):
     if TYPE_CHECKING:
         _token: str
         repo: Repository
+        bot_app_info: AppInfo
+        owner_id: int
 
     def __init__(self, bot_token: str, repo: Repository, command_prefix: str) -> None:
+        intents = discord.Intents.default()
+        intents.message_content = True
+
         super().__init__(
             command_prefix=command_prefix,
-            intents=discord.Intents.default(),
+            intents=intents,
             case_insensitive=True,
         )
         self.repo = repo
         self._token = bot_token
 
     async def setup_hook(self) -> None:
+        for extension in extensions:
+            try:
+                await self.load_extension(extension)
+            except Exception:
+                logger.exception(f"Failed to load extension {extension}.")
+
+        self.bot_app_info = await self.application_info()
+        self.owner_id = self.bot_app_info.owner.id
+
         await self.repo.sync()
         logger.info("Repository is ready.")
+
+        commands = await self.tree.sync()
+        logger.info(f"Successfully synced {len(commands)} commands'")
+
+    @property
+    def owner(self) -> User:
+        return self.bot_app_info.owner
 
     async def on_ready(self) -> None:
         logger.info(f"Logged on as {self.user}")
