b43505a1759efcd7023afeb839df45d645561946
Yumax-panda <yuma0wall@gmail.com>
2025-07-18 15:02:16 +0900

    HelpCommandのテンプレを作成

--- cogs/meta.py
+++ cogs/meta.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from discord.ext import commands
 
@@ -11,7 +11,21 @@ if TYPE_CHECKING:
 
 
 # TODO
-class HelpCommand(commands.HelpCommand): ...
+class HelpCommand(commands.HelpCommand):
+    if TYPE_CHECKING:
+        context: commands.Context[Bot]
+
+    def __init__(self) -> None:
+        super().__init__(
+            command_attrs={
+                "cooldown": commands.CooldownMapping.from_cooldown(
+                    1, 3.0, commands.BucketType.member
+                ),
+                "help": "Botやコマンドについてのヘルプを表示",
+            }
+        )
+
+    async def send_bot_help(self, _: Any) -> None: ...
 
 
 class Meta(Cog, name="Meta", description="便利機能"):
