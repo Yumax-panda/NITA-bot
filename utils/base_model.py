52d6bd8959e7ea356e506143da89207b258ac197
Yumax-panda <yuma0wall@gmail.com>
2025-07-18 15:03:57 +0900

    _parse_valueは呼び出されることを想定していないため，メソッドを切り出した

--- utils/base_model.py
+++ utils/base_model.py
@@ -4,6 +4,29 @@ from typing import Any, TypeVar, get_args, get_origin, get_type_hints
 T = TypeVar("T", bound="BaseModel")
 
 
+def _parse_value(field_type: type, value: Any) -> Any:
+    origin = get_origin(field_type)
+
+    if origin is not None:
+        # Nested generics (already handled in from_dict)
+        return value
+
+    # datetime
+    if issubclass(field_type, datetime):
+        return datetime.fromisoformat(value)
+
+    # str, int, float, bool
+    if issubclass(field_type, str | int | float | bool):
+        return field_type(value)
+
+    # Nested custom class
+    if hasattr(field_type, "from_dict") and callable(field_type.from_dict):
+        return field_type.from_dict(value)
+
+    # fallback
+    return value
+
+
 class BaseModel:
     __slots__ = ()
 
@@ -24,40 +47,17 @@ class BaseModel:
             # List[T]
             if origin is list:
                 elem_type = get_args(field_type)[0]
-                init_kwargs[key] = [cls._parse_value(elem_type, v) for v in value]
+                init_kwargs[key] = [_parse_value(elem_type, v) for v in value]
 
             # Dict[K, V]
             elif origin is dict:
                 key_type, val_type = get_args(field_type)
                 init_kwargs[key] = {
-                    cls._parse_value(key_type, k): cls._parse_value(val_type, v)
+                    _parse_value(key_type, k): _parse_value(val_type, v)
                     for k, v in value.items()
                 }
 
             else:
-                init_kwargs[key] = cls._parse_value(field_type, value)
+                init_kwargs[key] = _parse_value(field_type, value)
 
         return cls(**init_kwargs)
-
-    @staticmethod
-    def _parse_value(field_type: type, value: Any) -> Any:
-        origin = get_origin(field_type)
-
-        if origin is not None:
-            # Nested generics (already handled in from_dict)
-            return value
-
-        # datetime
-        if issubclass(field_type, datetime):
-            return datetime.fromisoformat(value)
-
-        # str, int, float, bool
-        if issubclass(field_type, str | int | float | bool):
-            return field_type(value)
-
-        # Nested custom class
-        if hasattr(field_type, "from_dict") and callable(field_type.from_dict):
-            return field_type.from_dict(value)
-
-        # fallback
-        return value
